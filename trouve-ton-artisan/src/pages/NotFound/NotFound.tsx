fcc17670cfca8271ecbb606469c0e76d6f0250b7
IdhZY <241080699+IdhZY@users.noreply.github.com>
2026-04-13 10:42:19 +0200

    fix

--- trouve-ton-artisan/src/pages/NotFound/NotFound.tsx
+++ trouve-ton-artisan/src/pages/NotFound/NotFound.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
-import "./NotFound.css";
+import "./NotFound.scss";
 
 function NotFound() {
   // Titre dynamique de page (WCAG 2.4.2)
